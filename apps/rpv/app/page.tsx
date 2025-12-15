01c947265c16cc05bf475d1388550fdaaebb2ae9
suntorn-ra <suntorn.ra@logicspark.com>
2025-12-15 11:41:20 +0700

    💄 update button labels for RPV and VPV pages

--- apps/rpv/app/page.tsx
+++ apps/rpv/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>RPV</h1>
-      <Button appName="RPV">Click me</Button>
+      <Button appName="RPV">Click me (RPV)</Button>
     </div>
   );
 }
